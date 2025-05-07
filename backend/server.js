9f87588ed467c141779fb1a0ed9da6ad36e86a8b
Yacine781 <yacineinfo78@gmail.com>
2025-05-07 16:49:32 +0200

    dockerfile : changement de port 5000 en 3000

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.get("/", (req, res) => {
 
 // Définir le port d'écoute du serveur
 //const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
