a452582780cb5cd532098c38d8346594a340f10f
Yacine781 <yacineinfo78@gmail.com>
2025-05-15 11:23:25 +0200

    update

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -7,13 +7,15 @@ import { Platform } from "react-native";
 
 // !!! UTILISEZ VOTRE ADRESSE IP LOCALE RÉELLE ICI !!!
 // (Assurez-vous que c'est bien 192.168.1.14 et qu'il y a des points, pas de point-virgule)
-const LOCAL_DEV_URL = "http://192.168.1.14:5000/api"; // Correction du point-virgule
+const LOCAL_DEV_URL = "http://192.168.27.65/api"; // Correction du point-virgule
+//const LOCAL_DEV_URL = "http://192.168.27.65:5000/api"; // Correction du point-virgule
+
 
 // URL pour l'émulateur Android (moins pertinent si vous testez sur physique)
 const ANDROID_EMULATOR_URL = "http://10.0.2.2:5000/api";
 
 // URL pour le navigateur web
-const WEB_URL = "http://localhost:5000/api";
+const WEB_URL = "http://192.168.27.65/api";
 
 // --- CHOISIR LA BONNE URL ---
 let API_URL;
